6d7cf2bd810f22a397a188d0a61331a74f6d7931
Omer Gul <momergul@gmail.com>
2019-05-13 18:49:56 -0700

    Implemented classifiers A-E without Kaiming initialization

--- models.py
+++ models.py
@@ -2,35 +2,169 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class MNIST_Baseline(nn.Module):
+class Classifier_A(nn.Module):
     '''
-    An implementation of the basic CNN architecture
-    Madry et al used for training a model robust to
-    adversarial attacks on MNIST.
+    An implementation of Classifier A used by Samangouei
+    et al in their paper "Defense-GANL Protecting Classifiers 
+    Against Adversarial Attacks Using Generative Models."
+
+    Weight initialization should be performed after initializing
+    the model
     '''
 
     def __init__(self):
         super().__init__()
 
-        # Initializing the conv layers with Kaiming and zero bias
-        self.conv1 = nn.Conv2d(1, 32, 5, bias=True)
-        nn.init.kaiming_normal_(self.conv1.weight)
-        nn.init.constant(self.conv1.bias, 0)
-
-        self.conv2 = nn.Conv2d(32, 64, 5, bias=True)
-        nn.init.kaiming_normal_(self.conv2.weight)
-        nn.init.constant(self.conv2.bias, 0)
+        # Initializing the conv layers
+        self.conv_1 = nn.Conv2d(1, 64, 5, padding=2, bias=True)
+        self.conv_2 = nn.Conv2d(64, 64, 5, stride=2, bias=True)
 
         # Initializing the fully connected layer
-        self.fc = nn.Linear(1024, 10, bias=True)
-        nn.inint.kaiming_normal_(self.fc.weight)
-        nn.init.constant(self.fc.bias, 0)
+        self.drop_1 = nn.Dropout(p=0.25)
+        self.fc_1 = nn.Linear(9216, 128, bias=True)
+
+        self.drop_2 = nn.Dropout(p=0.5)
+        self.fc_2 = nn.Linear(128, 10, bias=True)
 
     def forward(self, x):
-        hidden_1 = F.max_pool2d(F.relu(self.conv1(x)), 2, stride=2)
-        hidden_2 = F.max_pool2d(F.relu(self.conv2(hidden_1)), 2, stride=2)
+        # Forward prop through conv layers
+        hidden_1 = F.relu(self.conv_1(x))
+        hidden_2 = F.relu(self.conv_2(hidden_1))
+
+        # Flatten image and apply dropout
+        N = hidden_2.shape[0]
+        hidden_2 = hidden_2.view(N, -1)
+        hidden_2 = self.drop_1(hidden_2)
+
+        # FC layers
+        hidden_3 = self.drop_2(F.relu(self.fc_1(hidden_2)))
+        out = self.fc_2(hidden_3)
+
+        return out
+
+class Classifier_B(nn.Module):
+    '''
+    An implementation of Classifier B used by Samangouei
+    et al in their paper "Defense-GANL Protecting Classifiers 
+    Against Adversarial Attacks Using Generative Models."
+
+    Weight initialization should be performed after initializing
+    the model
+    '''
+    
+    def __init__(self):
+        super().__init__()
+
+        self.drop_1 = nn.Dropout(p=0.2)
+        self.conv_1 = nn.Conv2d(1, 64, 8, stride=2, padding=5, bias=True)
+
+        self.conv_2 = nn.Conv2d(64, 128, 6, stride=2, bias=True)
+
+        self.conv_3 = nn.Conv2d(128, 128, 5, bias=True)
+        self.drop_2 = nn.Dropout(p=0.5)
+
+        self.fc = nn.Linear(1152, 10, bias=True)
+
+    def forward(self, x):
+        x = self.drop_1(x)
+        hidden_1 = F.relu(self.conv_1(x))
+        hidden_2 = F.relu(self.conv_2(hidden_1))        
+        hidden_3 = F.relu(self.conv_3(hidden_2))
+
+        # Flatten
+        N = hidden_3.shape[0]
+        hidden_3 = hidden_3.view(N, -1)
+        hidden_3 = self.drop_2(hidden_3)
+
+        out = self.fc(hidden_3)
+        return out
+
+class Classifier_C(nn.Module):
+    '''
+    An implementation of Classifier C used by Samangouei
+    et al in their paper "Defense-GANL Protecting Classifiers 
+    Against Adversarial Attacks Using Generative Models."
+
+    Weight initialization should be performed after initializing
+    the model
+    '''
+
+    def __init__(self):
+        super().__init__()
+
+        self.conv_1 = nn.Conv2d(1, 128, 3, padding=1, bias=True)
+        self.conv_2 = nn.Conv2d(128, 64, 3, stride=2, bias=True)
+
+        self.drop_1 = nn.Dropout(p=0.25)
+        self.fc_1 = nn.Linear(10816, 128, bias=True)
+        self.drop_2 = nn.Dropout(p=0.5)
+        self.fc_2 = nn.Linear(128, 10, bias=True)
+
+    def forward(self, x):
+        hidden_1 = F.relu(self.conv_1(x))
+        hidden_2 = F.relu(self.conv_2(hidden_1))
 
         N = hidden_2.shape[0]
         hidden_2 = hidden_2.view(N, -1)
+        hidden_2 = self.drop_1(hidden_2)
 
-        return self.fc(hidden_2)
+        hidden_3 = self.drop_2(F.relu(self.fc_1(hidden_2)))
+        out = self.fc_2(hidden_3)
+        return out
+
+class Classifier_D(nn.Module):
+    '''
+    An implementation of Classifier D used by Samangouei
+    et al in their paper "Defense-GANL Protecting Classifiers 
+    Against Adversarial Attacks Using Generative Models."
+
+    Weight initialization should be performed after initializing
+    the model
+    '''
+    
+    def __init__(self):
+        super().__init__()
+
+        self.fc_1 = nn.Linear(784, 200, bias=True)
+        self.drop_1 = nn.Dropout(p=0.5)
+
+        self.fc_2 = nn.Linear(200, 200, bias=True)
+        self.drop_2 = nn.Dropout(p=0.5)
+
+        self.fc_3 = nn.Linear(200, 10, bias=True)
+
+    def forward(self, x):
+        N = x.shape[0]
+        x = x.view(N, -1)
+
+        hidden_1 = self.drop_1(F.relu(self.fc_1(x)))
+        hidden_2 = self.drop_2(F.relu(self.fc_2(hidden_1)))
+        out = self.fc_3(hidden_2)
+        return out
+
+class Classifier_E(nn.Module):
+    '''
+    An implementation of Classifier E used by Samangouei
+    et al in their paper "Defense-GANL Protecting Classifiers 
+    Against Adversarial Attacks Using Generative Models."
+
+    Weight initialization should be performed after initializing
+    the model
+    '''
+    
+    def __init__(self):
+        super().__init__()
+
+        self.fc_1 = nn.Linear(784, 200, bias=True)
+        self.fc_2 = nn.Linear(200, 200, bias=True)
+        self.fc_3 = nn.Linear(200, 10, bias=True)
+        
+    def forward(self, x):
+        N = x.shape[0]
+        x = x.view(N, -1)
+
+        hidden_1 = F.relu(self.fc_1(x))
+        hidden_2 = F.relu(self.fc_2(hidden_1))
+        out = self.fc_3(hidden_2)
+        return out
+
