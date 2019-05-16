1835d57bbc48a90d2c9182e15c62c8b38e1090a6
Peter Maldonado <phm@stanford.edu>
2019-05-15 23:19:53 -0700

    Initial vizualization functions.

---
+++ viz_utils.py
@@ -0,0 +1,22 @@
+import os
+
+import torch
+from torchvision.utils import save_image
+
+MAX_IMG = 8
+def compare_VAE(batch, recon_batch, batch_size, epoch, path):
+
+    with torch.no_grad():
+        n = min(batch.size(0), 8)
+        comparison = torch.cat([batch[:n],
+                               recon_batch.view(batch_size, 1, 28, 28)[:n]])
+        save_image(comparison.cpu(),
+                   os.path.join(path, '/reconstruction_' + str(epoch) + '.png', nrow=n))
+
+def sample_VAE(vae_model, device, epoch, path):
+    vae_model.eval()
+    with torch.no_grad():
+            sample = torch.randn(64, 20).to(device)
+            sample = vae_model.decode(sample).cpu()
+            save_image(sample.view(64, 1, 28, 28),
+                       os.path.join(path, '/sample_' + str(epoch) + '.png'))
